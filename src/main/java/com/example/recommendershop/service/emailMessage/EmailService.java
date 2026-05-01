0a180a3c09f0cf50e3ec16e8245a883b38dd8e31
VuDaiSon <cocokoco108@gmail.com>
2026-05-01 23:05:54 +0700

    fix sendResetPassword

--- src/main/java/com/example/recommendershop/service/emailMessage/EmailService.java
+++ src/main/java/com/example/recommendershop/service/emailMessage/EmailService.java
@@ -3,21 +3,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class EmailService {
+
+    private static final Logger log = LoggerFactory.getLogger(EmailService.class);
 
     @Autowired
     private JavaMailSender mailSender;
 
     public void sendResetPassword(String to, String link) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
 
-        SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject("Reset mật khẩu");
+            message.setText("Click link để reset mật khẩu:\n" + link);
 
-        message.setTo(to);
-        message.setSubject("Reset mật khẩu");
-        message.setText("Click link để reset mật khẩu:\n" + link);
+            mailSender.send(message);
 
-        mailSender.send(message);
+        } catch (Exception e) {
+            log.error("❌ Gửi email thất bại tới: {}", to, e);
+        }
     }
 }
