821b47dfb8535d5ea72da1781f6632335662b9e5
VuDaiSon <cocokoco108@gmail.com>
2026-05-01 23:43:18 +0700

    fix sendResetPassword

--- src/main/java/com/example/recommendershop/service/emailMessage/EmailService.java
+++ src/main/java/com/example/recommendershop/service/emailMessage/EmailService.java
@@ -1,55 +1,31 @@
 package com.example.recommendershop.service.emailMessage;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 @Service
 public class EmailService {
 
     private static final Logger log = LoggerFactory.getLogger(EmailService.class);
 
-    @Value("${RESEND_API_KEY}")
-    private String apiKey;
+    @Autowired
+    private JavaMailSender mailSender;
 
     public void sendResetPassword(String to, String link) {
         try {
-            URL url = new URL("https://api.resend.com/emails");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            SimpleMailMessage message = new SimpleMailMessage();
 
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Authorization", "Bearer " + apiKey);
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setDoOutput(true);
+            message.setTo(to);
+            message.setSubject("Reset mật khẩu");
+            message.setText("Click link để reset mật khẩu:\n" + link);
 
-            String body = """
-                {
-                  "from": "onboarding@resend.dev",
-                  "to": ["%s"],
-                  "subject": "Reset mật khẩu",
-                  "html": "<p>Click vào link để reset mật khẩu:</p><a href='%s'>Reset Password</a>"
-                }
-                """.formatted(to, link);
-
-            try (OutputStream os = conn.getOutputStream()) {
-                os.write(body.getBytes());
-            }
-
-            int responseCode = conn.getResponseCode();
-
-            if (responseCode >= 200 && responseCode < 300) {
-                log.info("✅ Email sent to {}", to);
-            } else {
-                log.error("❌ Resend failed with code: {}", responseCode);
-            }
+            mailSender.send(message);
 
         } catch (Exception e) {
-            log.error("❌ Send email error", e);
+            log.error("❌ Gửi email thất bại tới: {}", to, e);
         }
     }
 }
