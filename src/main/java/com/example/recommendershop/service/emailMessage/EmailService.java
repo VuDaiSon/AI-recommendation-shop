0e445573ed7c6a5c8d01572f9f1233b166c9f9e3
VuDaiSon <cocokoco108@gmail.com>
2026-05-02 01:20:35 +0700

    fix sendResetPassword

--- src/main/java/com/example/recommendershop/service/emailMessage/EmailService.java
+++ src/main/java/com/example/recommendershop/service/emailMessage/EmailService.java
@@ -20,25 +20,25 @@ public class EmailService {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
 
-            helper.setFrom("Recommender Shop <tatbara2@gmail.com>");
+            helper.setFrom("Recommender Shop <a9e948001@smtp-brevo.com>");
             helper.setTo(to);
             helper.setSubject("Reset mật khẩu");
 
             String html = """
-                <div>
-                    <p>Click link để reset mật khẩu:</p>
-                    <a href="%s">%s</a>
-                </div>
-                """.formatted(link, link);
+            <div>
+                <p>Click link để reset mật khẩu:</p>
+                <a href="%s">%s</a>
+            </div>
+            """.formatted(link, link);
 
             helper.setText(html, true);
 
             mailSender.send(message);
 
-            log.info("✅ Email sent to {}", to);
+            log.info("✅ EMAIL SENT TO: {}", to);
 
         } catch (Exception e) {
-            log.error("❌ Send mail failed", e);
+            log.error("❌ EMAIL FAILED", e);
         }
     }
 }
